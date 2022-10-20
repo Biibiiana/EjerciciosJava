604dd71708811fe169d65c0aa24c77363ee5057c
Bibiana Antequera <98453779+Biibiiana@users.noreply.github.com>
2022-10-20 22:48:16 +0200

    Delete Tema 1/WS5 directory

--- Tema 1/WS5/WS5-1.js
+++
@@ -1,8 +0,0 @@
-number = parseInt(prompt("Introduce un número: "));
-
-if (number % 2 == 0) {
-    console.log("El número es par.");
-}
-else {
-    console.log("El número es impar.");
-}
--- Tema 1/WS5/WS5-2.js
+++
@@ -1,8 +0,0 @@
-age = parseInt(prompt("Introduzca su edad: "));
-
-if (age >= 18) {
-    console.log("El usuario es mayor de edad.");
-}
-else {
-    console.log("El usuario es menor de edad.");
-}
